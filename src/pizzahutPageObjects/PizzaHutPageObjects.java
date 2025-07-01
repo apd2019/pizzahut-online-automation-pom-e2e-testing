e31c6c1de544c03308057cc803eef63718bdde21
apd2019 <apd102018@gmail.com>
2025-07-01 21:37:18 +0530

    page-object-model

--- src/pizzahutPageObjects/PizzaHutPageObjects.java
+++ src/pizzahutPageObjects/PizzaHutPageObjects.java
@@ -1,1 +1,264 @@
+package pizzahutPageObjects;
 
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class PizzaHutPageObjects {
+	WebDriver driver;
+	WebDriverWait wait;
+
+	// Pop up black screen
+	@FindBy(xpath = "//div[@class='pl-10 pr-10']")
+	public WebElement autoLocationBlackPopUp;
+
+	// Pop up close button
+	@FindBy(xpath = "//button[@class='icon-close--white p-30 absolute top-0 right-0 mr-10 mt-10']")
+	public WebElement locationPopUpClsbtn;
+
+	// Location input
+	@FindBy(xpath = "//input[contains(@class,'search--hut')]")
+	public WebElement locationInput;
+
+	// First auto-populate dropdown option
+	@FindBy(xpath = "//div[@class='pt-5 border-t overflow-scrolling-touch']")
+	public List<WebElement> locationDropdownOptions;
+
+	@FindBy(xpath = "(//button[starts-with(@id, 'PlacesAutocomplete')])[1]")
+	public WebElement firstAutoPopulateOption;
+
+	@FindBy(xpath = "//button[contains(@class,'text-center')]")
+	public WebElement startOrderTimeBtn;
+
+	// Deals page navigation
+	@FindBy(xpath = "(//a[@href='/order/deals/'])[2]")
+	public WebElement dealsPageLink;
+
+	// Vegetarian radio button
+	@FindBy(xpath = "(//span[contains(@class, 'py-4 px-5 border rounded-full flex items-center')])[1]")
+	public WebElement vegetarianRadioBtnFlag;
+
+	// Pizzas menu bar option
+	@FindBy(xpath = "(//a[@href='/order/pizzas/'])[2]")
+	public WebElement pizzasMenuBarOption;
+
+	// Pizza containers (Recommended)
+	@FindBy(css = ".sc-fznXWL.sc-fznXWL.product-grid")
+	public List<WebElement> pizzaContainers;
+
+	// Add button inside pizza (relative, so handled in method)
+	// Basket
+	@FindBy(id = "basket")
+	public WebElement yourBasket;
+
+	@FindBy(xpath = "//div[contains(@class, 'basket-item-product-title')]")
+	public WebElement basketItemProductTitle;
+
+	@FindBy(xpath = "//div[contains(@class,'basket-item-product-price')]")
+	public WebElement basketItemProductPrice;
+
+	@FindBy(xpath = "//div[@class='text-12']/div[contains(@class,'supplement-value')]")
+	public WebElement restaurantCharges;
+
+	@FindBy(xpath = "(//div[contains(@class,'items-start')]/span)[4]")
+	public WebElement taxAmount;
+
+	@FindBy(xpath = "//a/span/span[contains(@data-synth,'basket-value')]")
+	public WebElement basketValue;
+
+	@FindBy(xpath = "//span/span[contains(@class,'bg-green-dark ')]")
+	public WebElement checkoutBtnItemText;
+
+	@FindBy(xpath = "//a[@href='/order/checkout/']/span[3]/span")
+	public WebElement totalPriceCount;
+
+	// Drinks option
+	@FindBy(xpath = "(//a[@href='/order/drinks/'])[2]")
+	public WebElement drinksOption;
+
+	// Drink containers
+	@FindBy(xpath = "//div[contains(@class,'product-grid')]")
+	public List<WebElement> drinkContainers;
+
+	// Add button for drink (handled in method)
+	@FindBy(xpath = "//button[contains(@class,'button--green')]/span/span[text()='Add']")
+	public WebElement addDrinkBtn;
+
+	// Remove pizza from basket
+	@FindBy(xpath = "(//button[contains(@class,'icon-close')])[1]")
+	public WebElement removePizzaBtn;
+
+	// Price tag removed from checkout
+	@FindBy(xpath = "(//span[contains(@class,'absolute')]/span)[2]")
+	public WebElement priceTagRemoved;
+
+	// Checkout button
+	@FindBy(xpath = "(//span[contains(@class,'absolute')]/span)[2]")
+	public WebElement checkoutButton;
+
+	// Minimum order required popup
+	@FindBy(xpath = "//div[contains(@class,'pt-20 bg-white')]")
+	public WebElement minOrderPopup;
+
+	public PizzaHutPageObjects(WebDriver driver) {
+		this.driver = driver;
+		this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		PageFactory.initElements(driver, this);
+	}
+
+	// Wait for auto location black pop up
+	public void waitForAutoLocationBlackPopUp() {
+		wait.until(ExpectedConditions.visibilityOf(autoLocationBlackPopUp));
+	}
+
+	// Close pop up
+	public void closePopUp() {
+		locationPopUpClsbtn.click();
+	}
+
+	// Wait for location input
+	public void waitForLocationInput() {
+		wait.until(ExpectedConditions.visibilityOf(locationInput));
+	}
+
+	// Type address
+	public void typeAddress(String address) {
+		locationInput.sendKeys(address);
+	}
+
+	// Select first auto populate dropdown option
+	public void selectFirstAutoPopulateDropdownOption() {
+		wait.until(ExpectedConditions
+				.presenceOfElementLocated(By.xpath("(//button[starts-with(@id, 'PlacesAutocomplete')])[1]")));
+		wait.until(ExpectedConditions.elementToBeClickable(firstAutoPopulateOption));
+		if (!locationDropdownOptions.isEmpty()) {
+			locationDropdownOptions.get(0).click();
+		} else {
+			throw new RuntimeException("No options found");
+		}
+		try {
+			wait.until(
+					ExpectedConditions.presenceOfElementLocated(By.xpath("//button[contains(@class,'text-center')]")));
+			startOrderTimeBtn.click();
+		} catch (Exception e) {
+			// ignore
+		}
+	}
+
+	// Wait for deals page
+	public void waitForDealsPage() {
+		wait.until(ExpectedConditions.visibilityOf(dealsPageLink));
+	}
+
+	// Validate vegetarian radio button flag is off
+	public boolean isVegetarianRadioBtnFlagOff() {
+		return !vegetarianRadioBtnFlag.isSelected();
+	}
+
+	// Click pizzas menu bar option
+	public void clickPizzasMenuBarOption() {
+		pizzasMenuBarOption.click();
+	}
+
+	// Select add button of any pizza from Recommended
+	public void selectAddButtonOfAnyPizzaFromRecommended(String targetPizzaName) {
+		wait.until(
+				ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector(".sc-fznXWL.sc-fznXWL.product-grid")));
+		for (WebElement pizza : pizzaContainers) {
+			if (pizza.getText().trim().contains(targetPizzaName)) {
+				WebElement addBtn = pizza.findElement(By.xpath(".//button/span/span[text()='Add']"));
+				wait.until(ExpectedConditions.elementToBeClickable(addBtn)).click();
+				return;
+			}
+		}
+		throw new RuntimeException("No matching pizza found");
+	}
+
+	// Get basket item product title
+	public String getBasketItemProductTitle() {
+		wait.until(ExpectedConditions
+				.presenceOfElementLocated(By.xpath("//div[contains(@class, 'basket-item-product-title')]")));
+		return basketItemProductTitle.getText();
+	}
+
+	// Get pizza price, restaurant charges, tax, total
+	public String getPizzaPrice() {
+		return basketItemProductPrice.getText();
+	}
+
+	public String getRestaurantCharges() {
+		return restaurantCharges.getText();
+	}
+
+	public String getTaxAmount() {
+		return taxAmount.getText();
+	}
+
+	public String getTotalPriceBefore() {
+		return basketValue.getText();
+	}
+
+	// Get checkout button item count
+	public String getCheckoutBtnItemText() {
+		return checkoutBtnItemText.getText();
+	}
+
+	// Get total price count
+	public String getTotalPriceCount() {
+		return totalPriceCount.getText();
+	}
+
+	// Click drinks option
+	public void clickDrinksOption() {
+		drinksOption.click();
+	}
+
+	// Select Pepsi option to add into the Basket
+	public void selectPepsiOptionToAddIntoBasket(String targetDrinkName) {
+		wait.until(
+				ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//div[contains(@class,'product-grid')]")));
+		for (WebElement drink : drinkContainers) {
+			if (drink.getText().contains(targetDrinkName)) {
+				wait.until(ExpectedConditions.elementToBeClickable(addDrinkBtn)).click();
+				return;
+			}
+		}
+		throw new RuntimeException("No drink found");
+	}
+
+	// Remove pizza from basket
+	public void removePizzaFromBasket() {
+		removePizzaBtn.click();
+	}
+
+	// Wait for price tag to be removed from checkout button
+	public void waitForPriceTagRemoved() {
+		wait.until(ExpectedConditions
+				.invisibilityOfElementLocated(By.xpath("//a/span/span[contains(@data-synth,'basket-value')]")));
+		wait.until(
+				ExpectedConditions.presenceOfElementLocated(By.xpath("(//span[contains(@class,'absolute')]/span)[2]")));
+	}
+
+	// Get price tag removed text
+	public String getPriceTagRemovedText() {
+		return priceTagRemoved.getText();
+	}
+
+	// Click checkout button
+	public void clickCheckoutButton() {
+		checkoutButton.click();
+	}
+
+	// Wait for minimum order required popup
+	public boolean isMinOrderPopupDisplayed() {
+		wait.until(ExpectedConditions.visibilityOf(minOrderPopup));
+		return minOrderPopup.isDisplayed();
+	}
+}
--- src/stepDefinitions/StepDefinitions.java
+++ src/stepDefinitions/StepDefinitions.java
@@ -1,5 +1,8 @@
 package stepDefinitions;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
 import static org.testng.Assert.assertFalse;
 
 import java.time.Duration;
@@ -18,12 +21,13 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import static org.junit.Assert.*;
+import pizzahutPageObjects.PizzaHutPageObjects;
 
 public class StepDefinitions {
 
 	public static WebDriver driver;
 	WebDriverWait wait;
+	PizzaHutPageObjects pizzaHutPage;
 
 	String pizzaPrice, restaurantCharges, taxAmount, totalPriceBefore, totalPriceAfter, totalPriceAfter1;
 
@@ -36,14 +40,14 @@ public class StepDefinitions {
 		WebDriverManager.chromedriver().clearDriverCache().setup();
 		driver.manage().window().maximize();
 		driver.get(url);
-
+		pizzaHutPage = new PizzaHutPageObjects(driver);
 	}
 
 	@When("User wait for auto location black pop up screen")
 	public void user_wait_for_auto_location_black_pop_up_screen() throws InterruptedException {
 		Thread.sleep(2000);
 		try {
-			wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='pl-10 pr-10']")));
+			pizzaHutPage.waitForAutoLocationBlackPopUp();
 		} catch (Exception e) {
 			System.out.println(e);
 		}
@@ -52,11 +56,8 @@ public class StepDefinitions {
 	@Then("User close the pop up screen")
 	public void user_close_the_pop_up_screen() throws InterruptedException {
 		try {
-			WebElement locationPopUpClsbtn = driver.findElement(
-					By.xpath("//button[@class='icon-close--white p-30 absolute top-0 right-0 mr-10 mt-10']"));
-			locationPopUpClsbtn.click();
+			pizzaHutPage.closePopUp();
 		} catch (Exception e) {
-
 			System.out.println(e);
 		}
 	}
@@ -64,56 +65,30 @@ public class StepDefinitions {
 	@And("User see pop up for delivery asking for enter location")
 	public void user_see_pop_up_for_delivery_asking_for_enter_location() throws InterruptedException {
 		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-		locationInput = wait.until(
-				ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[contains(@class,'search--hut')]")));
-
+		pizzaHutPage.waitForLocationInput();
 	}
 
 	WebElement locationInput;
 
 	@Then("User type address as {string}")
 	public void user_type_address_as(String location) throws InterruptedException {
-		locationInput.sendKeys(location);
+		pizzaHutPage.typeAddress(location);
 	}
 
 	@And("User select first auto populate drop down option")
 	public void user_select_first_auto_populate_drop_down_option() {
-
-		By locList = By.xpath("//div[@class='pt-5 border-t overflow-scrolling-touch']");
-		List<WebElement> options = driver.findElements(locList);
-		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-		wait.until(ExpectedConditions
-				.presenceOfElementLocated(By.xpath("(//button[starts-with(@id, 'PlacesAutocomplete')])[1]")));
-		wait.until(ExpectedConditions
-				.elementToBeClickable(By.xpath("(//button[starts-with(@id, 'PlacesAutocomplete')])[1]")));
-		if (!options.isEmpty()) {
-			options.get(0).click();
-			System.out.println("selected the first location in the dropdown");
-		} else {
-			throw new NoSuchElementException("No options found");
-		}
-
-		try {
-			WebElement startOrdrTm = wait.until(
-					ExpectedConditions.presenceOfElementLocated(By.xpath("//button[contains(@class,'text-center')]")));
-			startOrdrTm.click();
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-
+		pizzaHutPage.selectFirstAutoPopulateDropdownOption();
 	}
 
 	@When("User navigate to deails page")
 	public void user_navigate_to_deails_page() throws InterruptedException {
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//a[@href='/order/deals/'])[2]")));
+		pizzaHutPage.waitForDealsPage();
 	}
 
 	@Then("User validate vegetarian radio button flag is off")
 	public void user_validate_vegetarian_radio_button_flag_is_off() throws InterruptedException {
-		WebElement radioBtnFlag = driver.findElement(
-				By.xpath("(//span[contains(@class, 'py-4 px-5 border rounded-full flex items-center')])[1]"));
-		assertFalse(radioBtnFlag.isSelected());
-		if (!radioBtnFlag.isSelected()) {
+		assertFalse(pizzaHutPage.vegetarianRadioBtnFlag.isSelected());
+		if (!pizzaHutPage.vegetarianRadioBtnFlag.isSelected()) {
 			System.out.println("radio button is off");
 		} else {
 			System.out.println("radio button is enabled");
@@ -122,51 +97,28 @@ public class StepDefinitions {
 
 	@And("User clicks on Pizzas menu bar option")
 	public void user_clicks_on_pizzas_menu_bar_option() {
-		WebElement pizzaMenu = driver.findElement(By.xpath("(//a[@href='/order/pizzas/'])[2]"));
-		pizzaMenu.click();
-		System.out.println(pizzaMenu.getText());
+		pizzaHutPage.clickPizzasMenuBarOption();
+		System.out.println(pizzaHutPage.pizzasMenuBarOption.getText());
 	}
 
 	@When("User select add button of any pizza from Recommended")
 	public void user_select_add_button_of_any_pizza_from_recommended() {
 		String targetPizzaName = "Southern Fiery Chicken";
-		wait.until(
-				ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector(".sc-fznXWL.sc-fznXWL.product-grid")));
-		List<WebElement> pizzaContainer = driver.findElements(By.cssSelector(".sc-fznXWL.sc-fznXWL.product-grid"));
-		System.out.println("outer for loop is executed");
-
-		for (WebElement pizza : pizzaContainer) {
-
-			System.out.println("for loop is execd" + ": Pizza Names: " + pizza.getText());
-			if (pizza.getText().trim().contains(targetPizzaName)) {
-				WebElement addBtn = pizza.findElement(By.xpath("//button/span/span[text()='Add']"));
-				wait.until(ExpectedConditions.elementToBeClickable(addBtn)).click();
-				System.out.println("this is executed");
-				return;
-			} else {
-				System.out.println("no match found");
-			}
-		}
+		pizzaHutPage.selectAddButtonOfAnyPizzaFromRecommended(targetPizzaName);
 	}
 
 	@Then("User see that the pizza is getting added under Your Basket")
 	public void user_see_that_the_pizza_is_getting_added_under_your_basket() {
-		WebElement yourBasket = driver.findElement(By.id("basket"));
-		wait.until(ExpectedConditions
-				.presenceOfElementLocated(By.xpath("//div[contains(@class, 'basket-item-product-title')]")));
-		WebElement basketItem = yourBasket
-				.findElement(By.xpath("//div[contains(@class, 'basket-item-product-title')]"));
-		System.out.println(basketItem.getText());
+		pizzaHutPage.getBasketItemProductTitle();
+		System.out.println(pizzaHutPage.basketItemProductTitle.getText());
 	}
 
 	@And("User validate pizza price plus Tax is checkout price")
 	public void user_validate_pizza_price_plus_tax_is_checkout_price() {
-		pizzaPrice = driver.findElement(By.xpath("//div[contains(@class,'basket-item-product-price')]")).getText();
-		restaurantCharges = driver
-				.findElement(By.xpath("//div[@class='text-12']/div[contains(@class,'supplement-value')]")).getText();
-		taxAmount = driver.findElement(By.xpath("(//div[contains(@class,'items-start')]/span)[4]")).getText();
-		totalPriceBefore = driver.findElement(By.xpath("//a/span/span[contains(@data-synth,'basket-value')]"))
-				.getText();
+		pizzaPrice = pizzaHutPage.getPizzaPrice();
+		restaurantCharges = pizzaHutPage.getRestaurantCharges();
+		taxAmount = pizzaHutPage.getTaxAmount();
+		totalPriceBefore = pizzaHutPage.getTotalPriceBefore();
 
 		System.out.println("PizzaPrice: " + pizzaPrice);
 		System.out.println("Restaurant Handling Charges: " + restaurantCharges);
@@ -191,57 +143,38 @@ public class StepDefinitions {
 
 	@Then("User validate checkout button contains Item count")
 	public void user_validate_checkout_button_contains_item_count() {
-		WebElement checkoutBtnItemText = driver.findElement(By.xpath("//span/span[contains(@class,'bg-green-dark ')]"));
-		String itemText = checkoutBtnItemText.getText();
+		String itemText = pizzaHutPage.getCheckoutBtnItemText();
 		assertTrue(itemText.contains("1 item"));
 		System.out.println("item count text - passed!");
 	}
 
 	@And("User validate checkout button contains total price count")
 	public void user_validate_checkout_button_contains_total_price_count() {
-		{
-			String totalPriceCount = driver.findElement(By.xpath("//a[@href='/order/checkout/']/span[3]/span"))
-					.getText();
-			System.out.println("Total Price: " + totalPriceCount);
-			assertTrue(totalPriceCount.contains(totalPriceBefore));
-
-		}
-
+		String totalPriceCount = pizzaHutPage.getTotalPriceCount();
+		System.out.println("Total Price: " + totalPriceCount);
+		assertTrue(totalPriceCount.contains(totalPriceBefore));
 	}
 
 	@Then("User clicks on Drinks option")
 	public void user_clicks_on_drinks_option() {
-		driver.findElement(By.xpath("(//a[@href='/order/drinks/'])[2]")).click();
+		pizzaHutPage.clickDrinksOption();
 	}
 
 	@And("User select Pepsi option to add into the Basket")
 	public void user_select_pepsi_option_to_add_into_the_basket() {
 		String targetDrinkName = "Pepsi";
-		wait.until(
-				ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//div[contains(@class,'product-grid')]")));
-		List<WebElement> drinkContainer = driver.findElements(By.xpath("//div[contains(@class,'product-grid')]"));
-		for (WebElement drink : drinkContainer) {
-			if (drink.getText().contains(targetDrinkName)) {
-				WebElement addBtn = driver
-						.findElement(By.xpath("//button[contains(@class,'button--green')]/span/span[text()='Add']"));
-				wait.until(ExpectedConditions.elementToBeClickable(addBtn)).click();
-				return;
-			} else {
-				System.out.println("no drink found!");
-			}
-		}
+		pizzaHutPage.selectPepsiOptionToAddIntoBasket(targetDrinkName);
 	}
 
 	@Then("User see {int} items are showing under checkout button")
 	public void user_see_items_are_showing_under_checkout_button(Integer int1) {
-		assertTrue(driver.findElement(By.xpath("//span/span[contains(@class,'bg-green-dark')]")).getText()
-				.contains(int1 + " items"));
-		System.out.println(driver.findElement(By.xpath("//span/span[contains(@class,'bg-green-dark ')]")).getText());
+		assertTrue(pizzaHutPage.checkoutBtnItemText.getText().contains(int1 + " items"));
+		System.out.println(pizzaHutPage.checkoutBtnItemText.getText());
 	}
 
 	@And("User see total price is now more than before")
 	public void user_see_total_price_is_now_more_than_before() {
-		totalPriceAfter = driver.findElement(By.xpath("//a/span/span[contains(@data-synth,'basket-value')]")).getText();
+		totalPriceAfter = pizzaHutPage.getTotalPriceBefore();
 		assertNotEquals(totalPriceBefore, totalPriceAfter);
 		System.out.println("total before: " + totalPriceBefore + "total after: " + totalPriceAfter);
 		System.out.println("total price is now more than before!");
@@ -249,38 +182,30 @@ public class StepDefinitions {
 
 	@Then("User remove the Pizza item from Basket")
 	public void user_remove_the_pizza_item_from_basket() {
-		driver.findElement(By.xpath("(//button[contains(@class,'icon-close')])[1]")).click();
+		pizzaHutPage.removePizzaFromBasket();
 	}
 
 	@And("see Price tag got removed from the checkout button")
 	public void see_price_tag_got_removed_from_the_checkout_button() {
-		wait.until(ExpectedConditions
-				.invisibilityOfElementLocated(By.xpath("//a/span/span[contains(@data-synth,'basket-value')]")));
-		wait.until(
-				ExpectedConditions.presenceOfElementLocated(By.xpath("(//span[contains(@class,'absolute')]/span)[2]")));
-		assertFalse(driver.findElement(By.xpath("(//span[contains(@class,'absolute')]/span)[2]")).getText()
-				.contains(totalPriceBefore));
+		pizzaHutPage.waitForPriceTagRemoved();
+		assertFalse(pizzaHutPage.priceTagRemoved.getText().contains(totalPriceBefore));
 	}
 
 	@And("User see {int} item showing in checkout button")
 	public void user_see_item_showing_in_checkout_button(Integer int1) {
-		assertTrue(driver.findElement(By.xpath("//span/span[contains(@class,'bg-green-dark')]")).getText()
-				.contains(int1 + " item"));
-		System.out.println(driver.findElement(By.xpath("//span/span[contains(@class,'bg-green-dark ')]")).getText());
+		assertTrue(pizzaHutPage.checkoutBtnItemText.getText().contains(int1 + " item"));
+		System.out.println(pizzaHutPage.checkoutBtnItemText.getText());
 	}
 
 	@Then("User Clicks on Checkout button")
 	public void user_clicks_on_checkout_button() {
-		driver.findElement(By.xpath("(//span[contains(@class,'absolute')]/span)[2]")).click();
-
+		pizzaHutPage.clickCheckoutButton();
 	}
 
 	@And("User see minimum order required pop up is getting displayed")
 	public void user_see_minimum_order_required_pop_up_is_getting_displayed() {
-		WebElement minOrdPopup = wait.until(
-				ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@class,'pt-20 bg-white')]")));
-		assertTrue(minOrdPopup.isDisplayed());
-		System.out.println(minOrdPopup.isDisplayed());
+		assertTrue(pizzaHutPage.isMinOrderPopupDisplayed());
+		System.out.println(pizzaHutPage.minOrderPopup.isDisplayed());
 	}
 
 }
